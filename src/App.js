552d374b971556091bdf17d7a6b99871eead7545
JoaoVitor <joaovitorpessoal2@gmail.com>
2020-03-31 22:48:24 -0300

    add semantic-ui-css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="ui container">
+      <Route path="/" exact component={HomePage} />
+      <Route path="/login" exact component={LoginPage} />
     </div>
   );
 }
---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginPage = props => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+        </div>
+    )
+};
+
+export default LoginPage;
